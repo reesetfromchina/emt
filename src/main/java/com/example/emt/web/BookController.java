97865b15c7c5ea8929a85840222bad88668bd2dd
reesetfromchina <100435809+reesetfromchina@users.noreply.github.com>
2024-03-24 17:57:36 +0100

    added controller

---
+++ src/main/java/com/example/emt/web/BookController.java
@@ -0,0 +1,35 @@
+package com.example.emt.web;
+
+import com.example.emt.model.Book;
+import com.example.emt.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public String getProductPage(@RequestParam(required = false) String error, Model model) {
+        if (error != null && !error.isEmpty()) {
+            model.addAttribute("hasError", true);
+            model.addAttribute("error", error);
+        }
+        List<Book> books = this.bookService.findAll();
+        model.addAttribute("books", books);
+        // model.addAttribute("bodyContent", "books");
+        return "list.html";
+
+
+    }
+}
